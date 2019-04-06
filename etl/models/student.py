1625bdb6b6ac00efb3917eceb40bbba2a2197908
Jonny Waffles <mr.waffles@gmail.com>
2019-04-06 15:09:29 -0400

    Began flattening survey

--- etl/models/student.py
+++ etl/models/student.py
@@ -38,6 +38,7 @@ class StudentTermTimeSeries(Base):
     growth_eumerated_values = 'any projection exceeded'.split()
 
     id = Column(Integer, primary_key=True, autoincrement='auto')
+    student_id = Column(Integer, ForeignKey('StudentRoster.id'))
     time_series = Column(Integer, ForeignKey('PeterPaulTerm.id'))
     grade = Column(Integer)
     peter_paul_location = Column(Integer, ForeignKey('PeterPaulLocation.id'))
@@ -48,6 +49,52 @@ class StudentTermTimeSeries(Base):
     growth_in_math = Column(Enum(growth_eumerated_values))
     test_percentile_in_math = Column(Integer)
     met_national_norm_in_math = Column(Boolean)
+
+
+class StudentExperienceQuestionnaire(Base):
+    __tablename__ = 'student_experience_questionnaire'
+
+    id = Column(Integer, primary_key=True, autoincrement='auto')
+    student_id = Column(Integer, ForeignKey('StudentRoster.id'))
+    time_series = Column(Integer, ForeignKey('PeterPaulTerm.id'))
+    kids_friendly_with_each_other = Column(Integer)
+    does_unwanted_teasing = Column(Integer)
+    kids_treat_each_other_respect = Column(Integer)
+    you_have_good_friends = Column(Integer)
+Spring-2016 d. Do you have a lot of good friends here?
+Spring-2016 e. If you were upset, would other kids here try to help you?
+Spring-2016 f. Do the other kids here listen to you?
+Spring-2016 a. Do you like coming here?
+Spring-2016 b. Do you have fun when you're here?
+Spring-2016 c. Do you feel bored when you're here?
+Spring-2016 d. Can you always find things that you like to do here?
+Spring-2016 a. Do you learn new things?
+Spring-2016 b. Do you feel challenged <i>in a good way</i>?
+Spring-2016 c. Do you get to do things here that you have never done before?
+Spring-2016 a. Is there an adult here who is interested in what you think about things?
+Spring-2016 b. Is there an adult here you can talk to when you are upset?
+Spring-2016 c. Is there an adult here who helps you when you have a problem?
+Spring-2016 d. Is there an adult here who <u>you</u> will listen to and respect?
+Spring-2016 a. I like to read at home during my free time.
+Spring-2016 b. I enjoy reading when I'm at school.
+Spring-2016 c. I enjoy reading when I'm at this after-school program.
+Spring-2016 d. I'm good at reading.
+Spring-2016 e. I like to give new books a try, even if they look hard.
+Spring-2016 Has coming to this after-school program helped you to read more often?
+Spring-2016 a. I like to learn new things in math.
+Spring-2016 b. I like to do math when I'm at school.
+Spring-2016 c. I like to do math when I'm at this after-school program.
+Spring-2016 d. Math is something I'm good at.
+Spring-2016 e. I'm interested in math.
+Spring-2016 f. I like to give new math problems a try, even when they look hard.
+Spring-2016 Has coming to this after-school program helped you do better in math?
+Spring-2016 a. Coming here has helped me to get my homework done.
+Spring-2016 b. Coming here has helped me to try harder in school.
+Spring-2016 c. Coming here has helped me to do better in school.
+Spring-2016 a. Be successful in high school?
+Spring-2016 b. Graduate from high school?
+Spring-2016 c. Go to college?"""
+
 
 
 class StudentYearTimeSeries(Base):
