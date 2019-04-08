5716910ee4e832059889a1aad8aa4a158828cb7c
Jonny Waffles <mr.waffles@gmail.com>
2019-04-08 19:44:02 -0400

    Fixed enums added concept of time_series_sequence so ordinal position is not tied to the pk

--- etl/models/timeseries.py
+++ etl/models/timeseries.py
@@ -16,24 +16,37 @@ class BiannualObservation(Base):
 
     id = Column(Integer, primary_key=True, autoincrement='auto')
     year_id = Column(Integer, ForeignKey('year.id'))
-    season = Column(Enum('Spring Summer Winter Fall'.split()))
+    time_series_sequence = Column(Integer)
+    season = Column(Enum('Fall Spring'.split()))
 
 
 class QuarterlyObservation(Base):
-    """Time series for observations occuring each quarter."""
+    """Time series for observations occuring each quarter.
+    """
     __tablename__ = 'quarterly_observation'
+
+    season_values = 'Fall Winter Spring Summer'.split()
 
     id = Column(Integer, primary_key=True, autoincrement='auto')
     year_id = Column(Integer, ForeignKey('year.id'))
+    time_series_sequence = Column(Integer)
     quarter = Column(Integer)
     # todo unique together year_id and quarter
 
 
 class TrimesterObservation(Base):
-    """Time series for observations occuring three times each year."""
+    """Time series for observations occuring three times each year.
+
+    Beginning September of the school year and aligns with
+    marking periods.
+    """
     __tablename__ = 'trimester_observation'
 
+    season_values = 'Fall Winter Spring'.split()
+
     id = Column(Integer, primary_key=True, autoincrement='auto')
+    time_series_sequence = Column(Integer)
     year_id = Column(Integer, ForeignKey('year.id'))
     trimester = Column(Integer)
+    season = Column(Enum(season_values))
     # todo unique together year_id and  trimester
