ee13ba586c204c19a418637e2b943f46fadfc945
Fuller, Jonathan <jfuller@markelcorp.com>
2019-04-10 22:32:39 -0400

    Added the ability to scrape survey data from the Student roster, added a test excel sheet with fake data, and an excel configuration file for future use

--- etl/excel_ingestion/youthsurvey/test/test_historical.py
+++ etl/excel_ingestion/youthsurvey/test/test_historical.py
@@ -1,4 +1,20 @@
-from etl.excel_ingestion.youthsurvey.youthsurveyhistorical import question_re
+from etl.excel_ingestion.youthsurvey.youthsurveyhistorical import (
+    question_re, get_student_results, validate_and_get_question_metadata
+)
+from etl.resources import get_pkg_resource_path
+
+from openpyxl import load_workbook
+
+PACKAGE = 'etl.excel_ingestion.youthsurvey.test'
+
+
+def get_workbook(name: str):
+    p = get_pkg_resource_path(PACKAGE, name)
+    return load_workbook(p, read_only=True, data_only=True)
+
+
+def get_data(workbook):
+    return list(workbook['Students'].values)
 
 
 def test_regex():
@@ -16,3 +32,24 @@ def test_regex():
     for expected, question in zip(expected, (question_1, question_2, question_3)):
         match = question_re.match(question)
         assert match.groupdict() == expected
+
+
+def test_get_student_results():
+    wb = get_workbook('test_student_roster.xlsx')
+    data = get_data(wb)
+    question_metadata = validate_and_get_question_metadata(data)
+    dictionary = get_student_results(1, question_metadata, data)
+    answers = dictionary['78']
+    found = check_for_dict(answers,
+                           '2015',
+                           'Fall',
+                           'd. Is there an adult here who you will listen to and respect?',
+                           4)
+    assert found
+
+
+def check_for_dict(answers, year, season, question, answer):
+    for _, dictionary in enumerate(answers):
+        data = tuple(map(dictionary.get, 'year season question answer'.split()))
+        if data == (year, season, question, answer):
+            return dictionary
